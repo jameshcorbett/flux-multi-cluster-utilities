fa15d708771bc12b320b97dee953023349767fde
Naman Kulshreshtha <nkulshr@clemson.edu>
2026-06-11 17:01:59 -0700

    Use individual jobids for futures
    Currently the SOURCE instance jobid is stored via
    flux_jobtap_job_aux_set, so its lifecycle is tied to the SOURCE
    instance job. When a job is canceled in the SOURCE cluster, the jobid is
    freed once the job becomes INACTIVE. Code that later calls
    flux_future_aux_get to retrieve the jobid then receives a NULL pointer,
    leading to a use-after-free.
    
    Fix this by associating an individual jobid with each future, so the
    jobid's lifecycle matches the lifetime of the future that uses it.

--- src/job-manager/plugins/delegate.c
+++ src/job-manager/plugins/delegate.c
@@ -114,24 +114,33 @@ static void submit_callback (flux_future_t *f, void *arg)
 {
     flux_t *h, *delegated_h;
     flux_plugin_t *p = arg;
-    flux_jobid_t delegated_id, *orig_id;
+    flux_jobid_t delegated_id, *orig_id, *idcpy = NULL, *id_for_wait = NULL, *id_for_event = NULL;
+    ;
     flux_future_t *wait_future = NULL, *event_future = NULL;
-    const char *errstr;
 
+    const char *errstr;
     if (!(h = flux_jobtap_get_flux (p))) {
         flux_future_destroy (f);
         return;
-    } else if (!(orig_id = flux_future_aux_get (f, "flux::jobid"))) {
+    }
+    if (!(id_for_wait = malloc (sizeof (*id_for_wait)))
+        || !(id_for_event = malloc (sizeof (*id_for_event)))
+        || !(idcpy = malloc (sizeof (*idcpy)))) {
+        flux_log_error (h, "Unable to create flux_ids");
+    }
+    if (!(orig_id = flux_future_aux_get (f, "flux::jobid"))) {
         flux_log_error (h, "in submit callback: couldn't get jobid");
         flux_future_destroy (f);
         return;
     }
+    *id_for_event = *orig_id;
+    *id_for_wait = *orig_id;
     if (!(delegated_h = flux_future_get_flux (f)) || flux_job_submit_get_id (f, &delegated_id) < 0
         || !(wait_future = flux_job_wait (delegated_h, delegated_id))
-        || flux_future_aux_set (wait_future, "flux::jobid", orig_id, NULL) < 0
+        || flux_future_aux_set (wait_future, "flux::jobid", id_for_wait, free) < 0
         || flux_future_then (wait_future, -1, wait_callback, p) < 0
         || !(event_future = flux_job_event_watch (delegated_h, delegated_id, "eventlog", 0))
-        || flux_future_aux_set (event_future, "flux::jobid", orig_id, NULL) < 0
+        || flux_future_aux_set (event_future, "flux::jobid", id_for_event, free) < 0
         || flux_future_aux_set (event_future, "flux::handle", h, NULL) < 0
         || flux_future_then (event_future, -1, event_callback, p) < 0
         || flux_jobtap_event_post_pack (p,
@@ -144,12 +153,15 @@ static void submit_callback (flux_future_t *f, void *arg)
         if (!(errstr = flux_future_error_string (f))) {
             errstr = "";
         }
-        flux_log_error (h, "%s: submission to specified Flux instance failed", idf58 (*orig_id));
         flux_jobtap_raise_exception (p, *orig_id, "DelegationFailure", 0, errstr);
         flux_future_destroy (wait_future);
         flux_future_destroy (event_future);
         flux_future_destroy (f);
         return;
+    }
+    *idcpy = delegated_id;
+    if (flux_jobtap_job_aux_set (p, *orig_id, "flux::delegate::delegate_id", idcpy, free) < 0) {
+        flux_log_error (h, "unable to save delegated jobId");
     }
     flux_future_destroy (f);
 }
